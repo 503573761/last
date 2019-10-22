61c9dd4d6334f70294ceaf4e979badd4451c2a3c
Administrator <Administrator@PC-20190910BQFV>
2019-10-22 14:52:31 +0800

    1.01

---
+++ FinalReply/src/main/java/com/dzkj/star.java
@@ -0,0 +1,13 @@
+package com.dzkj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class star {
+public static void main(String[] args) {
+	SpringApplication.run(star.class, args);
+}
+}
